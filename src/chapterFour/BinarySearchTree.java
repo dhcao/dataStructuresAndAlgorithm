f88bd4f07c329a7aece587c8fde277b2810b28a3
dhcao <caohuan1112@163.com>
2019-01-20 22:30:43 +0800

    some code commit，nothing important。

--- src/chapterFour/BinarySearchTree.java
+++ src/chapterFour/BinarySearchTree.java
@@ -10,16 +10,17 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 
 	/**
 	 * 节点类
+	 *
 	 * @param <T>
 	 */
-	private static class BinaryNode<T>{
+	private static class BinaryNode<T> {
 
 		private T element;
 		private BinaryNode<T> left;
 		private BinaryNode<T> right;
 
-		BinaryNode(T t){
-			this(t,null,null);
+		BinaryNode(T t) {
+			this(t, null, null);
 		}
 
 		public BinaryNode(T t, BinaryNode<T> lt, BinaryNode<T> rt) {
@@ -35,40 +36,43 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 	/**
 	 * 构造函数
 	 */
-	public BinarySearchTree (){
+	public BinarySearchTree() {
 		root = null;
 	}
 
 	/**
 	 * 清空
 	 */
-	public void makeEmpty(){
+	public void makeEmpty() {
 		root = null;
 	}
 
 	/**
 	 * 判断树是否为空：只需要判断根节点是否为空即可。
+	 *
 	 * @return
 	 */
-	public Boolean isEmpty(){
+	public Boolean isEmpty() {
 		return root == null;
 	}
 
 	/**
-	 * 是否含有节点x
+	 * 是否含有节点x,含有则返回true，没有则返回fales
+	 *
 	 * @param x
 	 * @return
 	 */
-	public boolean contains(T x){
-		return contains(x,root);
+	public boolean contains(T x) {
+		return contains(x, root);
 	}
 
 	/**
 	 * 寻找最小值
+	 *
 	 * @return
 	 */
-	public T findMin(){
-		if(isEmpty()){
+	public T findMin() {
+		if (isEmpty()) {
 			throw new BufferUnderflowException();
 		}
 		return findMin(root).element;
@@ -76,10 +80,11 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 
 	/**
 	 * 寻找最小值
+	 *
 	 * @return
 	 */
-	public T findMax(){
-		if(isEmpty()){
+	public T findMax() {
+		if (isEmpty()) {
 			throw new BufferUnderflowException();
 		}
 		return findMax(root).element;
@@ -87,24 +92,26 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 
 	/**
 	 * 插入
+	 *
 	 * @param t
 	 */
-	public void insert(T t){
-		root = insert(t,root);
+	public void insert(T t) {
+		root = insert(t, root);
 	}
 
 	/**
 	 * 删除
+	 *
 	 * @param t
 	 */
-	public void remove(T t){
-		root = remove(t,root);
+	public void remove(T t) {
+		root = remove(t, root);
 	}
 
 	/**
 	 * 打印全部
 	 */
-	public void printTree(){
+	public void printTree() {
 		// todo
 	}
 
@@ -130,13 +137,30 @@ public class BinarySearchTree<T extends Comparable<? super T>> {
 		return null;
 	}
 
-	// todo
+	/**
+	 * 如果T是空集，那么可以就返回false。否则，存在T处的项是X，那么就可以返回ture，否则对树对左子树或右子树进行一次递归。
+	 *
+	 * @param x
+	 * @param root
+	 * @return
+	 */
 	private boolean contains(T x, BinaryNode<T> root) {
-		// todo
-		return Boolean.parseBoolean(null);
+
+		if (root == null) {
+			return false;
+		}
+
+		// 判断x是在左子树还是右子树
+		int compareResult = x.compareTo(root.element);
+
+		if (compareResult < 0) {
+			return contains(x, root.left);
+		} else {
+			return contains(x, root.right);
+		}
 	}
 
-	public void printTrees(BinaryNode<T> tb){
+	public void printTrees(BinaryNode<T> tb) {
 		// todo
 	}
 
