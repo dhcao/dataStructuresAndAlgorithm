106c371a26a8a5169168577e92e0f63df0e6b6fb
dhcao <caohuan1112@163.com>
2019-02-28 22:27:56 +0800

    add note;

--- src/chapterFive/SeparateChainingHashTable.java
+++ src/chapterFive/SeparateChainingHashTable.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * 散列表实现方法之一：分离链接法
+ * 分离链接法：在遇到冲突的时候，构建一个链表（linkedList）来存储，原位置存储链表位置。
  *
  * @Author: dhcao
  * @Version: 1.0
@@ -14,13 +16,13 @@ public class SeparateChainingHashTable<T> {
 	 * desc: 构造函数
 	 *
 	 * @return:
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	public SeparateChainingHashTable(){
+	public SeparateChainingHashTable() {
 		this(DEFAULT_TABLE_SIZE);
 	}
 
-	public SeparateChainingHashTable(int size){
+	public SeparateChainingHashTable(int size) {
 		theLists = new LinkedList[nextPrime(size)];
 
 		// 为表中每一个位置都创建一个表，在冲突时放入数据
@@ -32,9 +34,9 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc: 插入
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	public void insert(T t){
+	public void insert(T t) {
 		List<T> whichList = theLists[myhash(t)];
 
 		if (!whichList.contains(t)) {
@@ -49,9 +51,9 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc: 删除
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	public void remove(T t){
+	public void remove(T t) {
 		List<T> whichList = theLists[myhash(t)];
 		if (whichList.contains(t)) {
 			whichList.remove(t);
@@ -62,9 +64,9 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc: 是否含有
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	public boolean contains(T t){
+	public boolean contains(T t) {
 
 		// 在散列表中找到散列位置都链表
 		List<T> whichList = theLists[myhash(t)];
@@ -74,9 +76,9 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc: 清空散列表
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	public void makeEmpty(){
+	public void makeEmpty() {
 
 		// 清空表都每个位置
 		for (int i = 0; i < theLists.length; i++) {
@@ -97,12 +99,12 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc: 重新构造散列表（在散列表达到最大大小之后）
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	private void rehash(){
+	private void rehash() {
 		List<T>[] oldLists = theLists;
 
-		theLists = new List[nextPrime(2*theLists.length)];
+		theLists = new List[nextPrime(2 * theLists.length)];
 
 		for (int j = 0; j < theLists.length; j++) {
 			theLists[j] = new LinkedList<>();
@@ -120,15 +122,15 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc:获取hash值
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	private int myhash(T x){
+	private int myhash(T x) {
 
 		int hashVal = x.hashCode();
 
 		hashVal %= theLists.length;
 
-		if(hashVal < 0){
+		if (hashVal < 0) {
 			hashVal += theLists.length;
 		}
 
@@ -138,9 +140,9 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc:下一个素数
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	private static int nextPrime(int n){
+	private static int nextPrime(int n) {
 
 		boolean state = isPrime(n);
 		while (!state) {
@@ -152,21 +154,17 @@ public class SeparateChainingHashTable<T> {
 	/**
 	 * desc:是不是素数
 	 *
-	 * @auther:  dhcao
+	 * @auther: dhcao
 	 */
-	private static boolean isPrime(int n){
-		if (n == 2 || n == 3)
-		{
+	private static boolean isPrime(int n) {
+		if (n == 2 || n == 3) {
 			return true;
 		}
-		if (n % 6 != 1 && n % 6 != 5)
-		{
+		if (n % 6 != 1 && n % 6 != 5) {
 			return false;
 		}
-		for (int i = 5; i*i <= n; i += 6)
-		{
-			if (n % i == 0 || n % (i+2) == 0)
-			{
+		for (int i = 5; i * i <= n; i += 6) {
+			if (n % i == 0 || n % (i + 2) == 0) {
 				return false;
 			}
 		}
